0bcacc80d8caab1b5f2e30c24f4ba27369350e32
Innocious <innociousbliss@gmail.com>
2026-06-07 04:36:02 +0100

    feat: complete Module A user validation

--- internal/domain/models.go
+++ internal/domain/models.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"errors"
+	"strings"
 )
 
 type User struct {
@@ -15,9 +16,22 @@ func ValidateUser(u User) error {
 	if u.Age < 18 {
 		return errors.New("ERR_AGE_RESTRICTED")
 	}
+	if err := ValidateEmail(u.Email); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func ValidateEmail(email string) error {
+	if strings.Count(email, "@") != 1 {
+		return errors.New("ERR_INVALID_EMAIL")
+	}
+
+	splitEmail := strings.Split(email, "@")
+	if len(splitEmail[0]) == 0 || len(splitEmail[1]) == 0 {
+		return errors.New("ERR_INVALID_EMAIL")
+	}
+	
 	return nil
 }
